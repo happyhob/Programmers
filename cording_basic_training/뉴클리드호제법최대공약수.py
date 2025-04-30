0ebe11dd24fa035ec5ea824d114cbd21f2c44d47
hobinLee <happyhob24@gmail.com>
2025-04-30 23:36:51 +0900

    프로그래머스는 아니고 뉴클리드호제법을 구현해볼려고 함

---
+++ cording_basic_training/뉴클리드호제법최대공약수.py
@@ -0,0 +1,12 @@
+def gcd(a,b):
+    if a==0:
+        return b
+    if b==0:
+        return a
+    if a>=b:
+        return gcd(b,a%b)
+    else:
+        return gcd(a,b%a)
+    
+print(gcd(77,49))
+
