63656564172dbabf93dd4a1323a5e7c58ada25cf
HobinLee <102903029+happyhob@users.noreply.github.com>
2025-05-15 11:52:29 +0900

    Create 1로만들기.py

---
+++ cording_basic_training/1로만들기.py
@@ -0,0 +1,30 @@
+'''
+정수가 있을 때, 짝수라면 반으로 나누고, 홀수라면 1을 뺀 뒤 반으로 나누면, 마지막엔 1이 됩니다. 예를 들어 10이 있다면 다음과 같은 과정으로 1이 됩니다.
+
+10 / 2 = 5
+(5 - 1) / 2 = 2
+2 / 2 = 1
+위와 같이 3번의 나누기 연산으로 1이 되었습니다.
+
+정수들이 담긴 리스트 num_list가 주어질 때, num_list의 모든 원소를 1로 만들기 위해서 필요한 나누기 연산의 횟수를 return하도록 solution 함수를 완성해주세요.
+'''
+
+
+def solution(num_list):
+    count_list =[]
+    for num in num_list:
+        count =0
+        while(num!=1):
+            if num%2==0:
+                num = num/2
+            else:
+                num = (num-1)/2
+            count+=1
+        count_list.append(count)
+        
+    return sum(count_list)
+  
+'''
+def solution(num_list):
+    return sum(len(bin(i)) - 3 for i in num_list)
+'''
