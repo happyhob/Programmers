d2af155ae7c6f9647b261e9aad43b29bf23c21ae
HobinLee <102903029+happyhob@users.noreply.github.com>
2025-05-18 00:20:11 +0900

    Update 간단한논리연산.py

--- cording_basic_training/간단한논리연산.py
+++ cording_basic_training/간단한논리연산.py
@@ -6,3 +6,8 @@ boolean 변수 x1, x2, x3, x4가 매개변수로 주어질 때, 다음의 식의
 
 def solution(x1, x2, x3, x4):
     return (x1 or x2) and (x3 or x4)
+
+'''
+def solution(x1, x2, x3, x4):
+    return (x1 | x2) & (x3 | x4)
+'''
